a663463dac09e7d5ba0898ee30def52ad58694e4
jjanmo <jjanmo07@hanmail.net>
2024-01-24 23:06:45 +0900

    fix: update fetch-api and home-page

--- project01_app/src/app/api/connections/route.ts
+++ project01_app/src/app/api/connections/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
-import data from '@/app/api/data.json';
+import { openDb } from '@/lib/db';
+import { Connection } from '@/interface/connection';
 
 export async function GET() {
-  return NextResponse.json(data.connections);
+  const db = await openDb();
+  const connections = await db.all<Connection[]>('SELECT * from connections');
+
+  return NextResponse.json(connections);
 }
--- project01_app/src/app/page.tsx
+++ project01_app/src/app/page.tsx
@@ -1,50 +1,19 @@
 import { AVATAR_URL } from '@/constants/common';
 import Image from 'next/image';
 import Link from 'next/link';
+import { read } from '@/helper/fetch';
+import { Connection } from '@/interface/connection';
 
-export default function Home() {
-  const connections = [
-    {
-      id: 1,
-      name: 'Iosue Margareta',
-      avatarId: 10,
-      email: 'iosue00@connect.com',
-    },
-    {
-      id: 2,
-      name: 'Urban Kristine',
-      avatarId: 20,
-      email: 'kristine@connect.com',
-    },
-    {
-      id: 3,
-      name: 'Damjana Adhiambo',
-      avatarId: 30,
-      email: 'adhiambo@connect.com',
-    },
-    {
-      id: 4,
-      name: 'aaaa',
-      avatarId: 40,
-      email: 'aaa@connect.com',
-    },
-    {
-      id: 5,
-      name: 'bbbb',
-      avatarId: 50,
-      email: 'bbbb@connect.com',
-    },
-  ];
+export default async function Home() {
+  const response = await read({ url: '/api/connections' });
+  const data = (await response.json()) as Connection[];
 
   return (
     <div className="w-9/12 pt-10 mx-auto">
-      <h1 className="text-3xl uppercase text-center text-slate-300 mb-10">
-        This will be your hyper connect!
-      </h1>
-
+      <h1 className="text-3xl uppercase text-center text-slate-300 mb-10">This will be your hyper connect!</h1>
       <section>
         <ul className="grid grid-cols-5 gap-5 row-auto">
-          {connections.map(({ id, avatarId, name }) => (
+          {data.map(({ id, avatarId, nickname }) => (
             <li key={id} className="flex justify-center items-center flex-col">
               <Link href={`/connect/${id}`}>
                 <Image
@@ -55,7 +24,7 @@ export default function Home() {
                   alt="avatar"
                 />
               </Link>
-              <div className="text-center">{name}</div>
+              <div className="text-center">{nickname}</div>
             </li>
           ))}
         </ul>

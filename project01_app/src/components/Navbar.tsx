7573d8f27a36ea746bb38bff1eafe35ce90d4764
jjanmo <jjanmo07@hanmail.net>
2024-01-31 01:04:09 +0900

    cleanup

--- project01_app/src/components/Navbar.tsx
+++ project01_app/src/components/Navbar.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className="w-full h-20  bg-green-300">
+    <div className="font-mono w-full h-20  bg-emerald-300">
       <div className="w-1/2 h-full flex justify-between items-center mx-auto">
-        <div className="font-mono text-2xl text-white h-full flex items-center">
+        <div className="text-2xl text-white h-full flex items-center">
           <Link href="/">Random Connect</Link>
         </div>
-        <div className="font-serif italic text-2xl text-green-500 animate-pulse h-full flex items-center hover:scale-150 ease-out duration-500">
-          <Link href="/create">Create Connection 🌍</Link>
+        <div className="text-2xl text-emerald-500 h-full flex items-center uppercase hover:opacity-80 transition-all">
+          <Link href="/add">Add</Link>
         </div>
       </div>
     </div>
--- project01_app/src/constants/common.ts
+++ project01_app/src/constants/common.ts
@@ -1,1 +1,2 @@
 export const AVATAR_URL = 'https://i.pravatar.cc/300';
+// https://i.pravatar.cc/300?img=2

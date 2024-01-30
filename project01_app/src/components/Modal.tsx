a9cc6d4a250c506612a70965ee96cd932bfa39ac
jjanmo <jjanmo07@hanmail.net>
2024-01-31 00:52:12 +0900

    fix: update modal comp

--- project01_app/src/components/Modal.tsx
+++ project01_app/src/components/Modal.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import React, { PropsWithChildren } from 'react';
 
 interface Props {
-  children: React.ReactNode;
   onClose: () => void;
 }
 
-export function Modal({ children, onClose }: Props) {
+export default function Modal({ children, onClose }: PropsWithChildren<Props>) {
   return (
-    <div className="w-screen h-screen absolute top-0 left-0 flex justify-center items-center ">
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" />
-
-      <div className="relative w-1/2 h-1/2 text-white text-3xl bg-red-300 rounded-lg">
-        <div
-          className="absolute right-1/2 -top-16 cursor-pointer"
-          onClick={onClose}
-        >
-          <AiOutlineCloseCircle size={50} color="white" />
-        </div>
-        {children}
-      </div>
+    <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center ">
+      <div className="w-full h-full absolute top-0 left-0 bg-[rgba(0,0,0,0.5)] cursor-pointer" onClick={onClose}></div>
+      {children}
     </div>
   );
 }

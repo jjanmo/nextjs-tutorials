e197d9829935af0afb2a1fc0b1dce9dc41f07bd8
jjanmo <jjanmo07@hanmail.net>
2024-02-03 01:43:22 +0900

    fix: update layout(add buttons) so change to client component

--- project01_app/src/app/connect/[id]/page.tsx
+++ project01_app/src/app/connect/[id]/page.tsx
@@ -1,29 +1,59 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { ArrowLeft } from 'lucide-react';
 import { read } from '@/helper/fetch';
 import { Connection } from '@/interface/connection';
 import { AVATAR_URL, dataKeyMap } from '@/constants/common';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 interface Props {
   params: { id: string };
 }
 
-export default async function ConnectDetailPage({ params }: Props) {
+export default function ConnectDetailPage({ params }: Props) {
   const router = useRouter();
-  const response = await read({ url: `/api/connect/${params.id}` });
-  const data = (await response.json()) as Connection;
+  const [data, setData] = useState<Connection | null>(null);
 
   const isAvatar = (key: string) => key === 'avatarId';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await read({ url: `/api/connect/${params.id}` });
+      const data = (await response.json()) as Connection;
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
 
   const handleBackClick = () => {
     router.back();
   };
 
+  const handleDeletelick = () => {};
+
+  const handleEditClick = () => {
+    router.push(`/edit?id=${params.id}`);
+  };
+
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <section className="h-dvh flex flex-col justify-center items-center">
-      <table className="w-1/2 mt-10 text-xl">
+    <section className="h-dvh flex flex-col justify-center items-center py-10">
+      <div className="relative w-1/2 text-center">
+        <h2 className="py-8 text-lg">
+          <span className="text-sky-500 font-semibold text-3xl">{data?.nickname}</span> 's CONNECTION
+        </h2>
+        <div
+          className="absolute top-1/2 left-0 -translate-y-1/2 p-2 rounded-full hover:bg-slate-200 cursor-pointer"
+          onClick={handleBackClick}
+        >
+          <ArrowLeft size={36} />
+        </div>
+      </div>
+      <table className="w-1/2 text-xl">
         <tbody className="p-1">
           {Object.entries(data).map(([key, value], i) => (
             <tr key={i} className="flex w-full mb-5">
@@ -48,15 +78,18 @@ export default async function ConnectDetailPage({ params }: Props) {
           ))}
         </tbody>
       </table>
-      <div className="mt-6">
+      <div className="w-1/2 mt-6 mx-auto flex justify-center">
         <button
-          className="w-32 py-2 mx-2 rounded-lg bg-red-400 text-white text-lg hover:opacity-90 transition-opacity"
-          onClick={handleBackClick}
+          className="w-32 py-2 mx-1 rounded-lg bg-emerald-400 text-white text-lg hover:opacity-90 transition-opacity"
+          onClick={handleEditClick}
         >
-          Back
-        </button>
-        <button className="w-32 py-2 mx-2 rounded-lg bg-blue-400 text-white text-lg hover:opacity-90 transition-opacity">
           Edit
+        </button>
+        <button
+          className="w-32 py-2 mx-1 rounded-lg bg-red-400 text-white text-lg hover:opacity-90 transition-opacity"
+          onClick={handleDeletelick}
+        >
+          Delete
         </button>
       </div>
     </section>

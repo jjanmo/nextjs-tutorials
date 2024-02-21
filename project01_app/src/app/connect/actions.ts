48fd45e72ad3524465958d09cb82af988410f2bb
jjanmo <jjanmo07@hanmail.net>
2024-02-21 23:52:10 +0900

    fix: move action file

---
+++ project01_app/src/app/connect/actions.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { BASE_URL, requestOptions } from '@/constants/fetch';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+export const submitForAdd = async (formData: FormData) => {
+  const body = {
+    nickname: formData.get('nickname'),
+    email: formData.get('email'),
+    thumbnail: Number(formData.get('thumbnail')),
+  };
+
+  try {
+    await fetch(`${BASE_URL}/api/connect`, {
+      method: 'POST',
+      ...requestOptions,
+      body: JSON.stringify(body),
+    });
+  } catch {
+    console.error('error');
+  } finally {
+    revalidatePath('/connect');
+    redirect('/connect');
+  }
+};

c1b969d9161a57007e5a4431c84c29d3931a284f
jjanmo <jjanmo07@hanmail.net>
2024-02-26 22:39:23 +0900

    feat: 리액트쿼리 설치 및 초기 설정 추가

--- project01_app/src/app/layout.tsx
+++ project01_app/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import QueryProviders from './query-provider';
 import { PropsWithChildren } from 'react';
 import Navbar from '@/components/Navbar';
 import './globals.css';
@@ -6,10 +7,12 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html>
       <body>
-        <header>
-          <Navbar />
-        </header>
-        <main className="min-h-[calc(100vh-5rem)] w-full bg-slate-50">{children}</main>
+        <QueryProviders>
+          <header>
+            <Navbar />
+          </header>
+          <main className="min-h-[calc(100vh-5rem)] w-full bg-slate-50">{children}</main>
+        </QueryProviders>
       </body>
     </html>
   );

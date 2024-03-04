4dac55685afa11102ebcdd3b9f86decbec47a697
jjanmo <jjanmo07@hanmail.net>
2024-03-04 23:32:46 +0900

    feat: add popular movies component as use-client

--- project01_app/src/app/movies/page.tsx
+++ project01_app/src/app/movies/page.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+'use client';
+
+import { Movie } from '@/interface/movie';
+import React, { useEffect, useState } from 'react';
+
+const options = {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + process.env.NEXT_PUBLIC_API_TOKEN,
+  },
+};
 
 export default function MoviesPage() {
-  return <div>Movies Page</div>;
+  const [movies, setMovies] = useState<Movie[]>();
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch('https://api.themoviedb.org/3/movie/popular', options);
+      const data = await response.json();
+      setMovies(data.results);
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Popular Movies</h1>
+      <ul>{movies && movies.map((movie) => <li key={movie.id}>{movie.title}</li>)}</ul>
+    </div>
+  );
 }

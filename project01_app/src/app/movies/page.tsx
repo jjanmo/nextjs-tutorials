cf96689e653423b4fb14e5d213b0f80a26645bfd
jjanmo <jjanmo07@hanmail.net>
2024-03-11 22:22:51 +0900

    fix: isMovie를 넘기는 방식 수정

--- project01_app/src/app/movies/page.tsx
+++ project01_app/src/app/movies/page.tsx
@@ -9,17 +9,15 @@ export default async function MoviesPage() {
   );
   popularData.sort((a, b) => b.popularity - a.popularity);
 
-  console.log(popularData.length);
+  const isMovie = (data: Media): data is MovieType => {
+    return 'release_date' in data;
+  };
 
   return (
     <div className="min-h-[calc(100vh-5rem)] flex flex-col items-center px-[10%] py-10">
       <ul className="w-full grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-6">
-        {popularData && popularData.map((movie) => <Movie key={movie.id} {...movie} />)}
+        {popularData && popularData.map((movie) => <Movie key={movie.id} {...movie} isMovie={isMovie(movie)} />)}
       </ul>
     </div>
   );
 }
-
-export const isMovie = (data: Media): data is MovieType => {
-  return 'release_date' in data;
-};
--- project01_app/src/components/Movie.tsx
+++ project01_app/src/components/Movie.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import { isMovie } from '@/app/movies/page';
 import { TMDB_MOVIE_IMAGE_URL } from '@/constants/common';
 import { Media } from '@/interface/movie';
 import Link from 'next/link';
 import Badge from './Badge';
 import Bookmark from './Bookmark';
 
-export default function Movie(props: Media) {
-  const { id, poster_path } = props;
+export default function Movie(props: Media & { isMovie: boolean }) {
+  const { id, poster_path, isMovie } = props;
 
-  const badgeText = isMovie(props) ? 'MOVIE' : 'TV';
+  const badgeText = isMovie ? 'MOVIE' : 'TV';
 
   return (
     <li className="relative rounded-md shadow-md overflow-hidden">
       <Bookmark />
-      <Link href={`/movies/${id}`}>
+      <Link href={`/movies/${id}?type=${isMovie ? 'movie' : 'tv'}`}>
         <div
           style={{ '--bg-image': `url(${TMDB_MOVIE_IMAGE_URL}/original/${poster_path})` } as React.CSSProperties}
           className="bg-[image:var(--bg-image)] bg-cover bg-center w-full pt-[150%] object-contain active:opacity-50"

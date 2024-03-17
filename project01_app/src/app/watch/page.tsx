405e70bccd14c9b0b2153efb414f8c5b8f0715f9
jjanmo <jjanmo07@hanmail.net>
2024-03-17 22:30:36 +0900

    fix: fix responsive-grid

--- project01_app/src/app/watch/page.tsx
+++ project01_app/src/app/watch/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { getPopularMovies, getPopularTVs } from '@/apis/movies';
+import { getPopularMovies } from '@/apis/movie';
+import { getPopularTVs } from '@/apis/tv';
 import Movie from '@/components/watch/Movie';
 import { Media, Movie as MovieType } from '@/interface/movie';
 
 export default async function WatchPage() {
-  const popularData = await Promise.all([getPopularMovies({})]).then(
-    (
-      result // getPopularTVs({}) 나중에 넣기
-    ) => result.map((data) => data.results).flat()
+  const popularData = await Promise.all([getPopularMovies({}), getPopularTVs({})]).then((result) =>
+    result.map((data) => data.results).flat()
   );
   popularData.sort((a, b) => b.popularity - a.popularity);
 
@@ -16,8 +15,15 @@ export default async function WatchPage() {
   };
 
   return (
-    <div className="min-h-[calc(100vh-5rem)] flex flex-col items-center px-[10%] py-10">
-      <ul className="w-full grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-6">
+    <div className="h-full flex flex-col items-center py-10">
+      <ul
+        className="grid 
+          grid-cols-[repeat(4,minmax(100px,1fr))]
+          sm:grid-cols-[repeat(5,minmax(120px,1fr))] 
+          md:grid-cols-[repeat(5,minmax(160px,1fr))] 
+          xl:grid-cols-[repeat(8,minmax(140px,1fr))]  
+          gap-6"
+      >
         {popularData && popularData.map((movie) => <Movie key={movie.id} {...movie} isMovie={isMovie(movie)} />)}
       </ul>
     </div>

d1b6a448f054f21c5a3a9b645c0446a86c04f7d0
jjanmo <jjanmo07@hanmail.net>
2024-03-20 00:06:42 +0900

    fix: backdrop image를 Next Image 컴퍼넌트로 변경

--- project01_app/src/components/watch/VideoTrailer.tsx
+++ project01_app/src/components/watch/VideoTrailer.tsx
@@ -1,18 +1,22 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import PlayIcon from '@/components/icons/PlayIcon';
 import { MovieVideo } from '@/interface/movie';
-import { TMDB_MOVIE_IMAGE_URL } from '@/constants/common';
+import { BLUR_DATA_URL, TMDB_MOVIE_IMAGE_URL } from '@/constants/common';
 
 interface Props {
-  officialTrailer: MovieVideo;
+  videos?: MovieVideo[];
   backdropPath: string;
   title: string;
 }
 
-export default function VideoTrailer({ officialTrailer, backdropPath, title }: Props) {
+export default function VideoTrailer({ backdropPath, title, videos }: Props) {
   const [showTrailer, setShowTrailer] = useState(false);
+
+  // TODO 어떤 비디오를 보여줄지 정해야함(메인 비디오의 기준은?) & 없다면 무엇을 보여주면 될까?
+  const mainVideo = videos?.[0];
 
   const handlePlayButtonClick = () => {
     setShowTrailer(true);
@@ -20,11 +24,16 @@ export default function VideoTrailer({ officialTrailer, backdropPath, title }: P
 
   return (
     <div className="relative w-full">
-      <div
-        style={{ '--backdrop-image': `url('${TMDB_MOVIE_IMAGE_URL}/original${backdropPath}')` } as React.CSSProperties}
-        className={`relative w-full h-[450px] bg-[image:var(--backdrop-image)] bg-cover bg-center 
-        before:backdrop-filter before:absolute before:w-full before:h-full`}
-      >
+      <div className="relative w-full h-[450px]">
+        <Image
+          className={`object-cover w-full h-full filter brightness-50 grayscale-50 ${showTrailer && 'blur-[2px]'}`}
+          src={`${TMDB_MOVIE_IMAGE_URL}/original${backdropPath}')`}
+          alt={`${title}_backdrop`}
+          fill
+          priority
+          placeholder="blur"
+          blurDataURL={BLUR_DATA_URL}
+        />
         <button
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-[0.15]"
           onClick={handlePlayButtonClick}
@@ -37,8 +46,8 @@ export default function VideoTrailer({ officialTrailer, backdropPath, title }: P
           <div className="relative max-w-[1120px] h-full mx-auto">
             <iframe
               className="absolute top-0 left-0 w-full h-full mx-auto"
-              src={`https://www.youtube.com/embed/${officialTrailer.key}`}
-              title={officialTrailer.name}
+              src={`https://www.youtube.com/embed/${mainVideo?.key || ''}?autoplay=1&mute=1`}
+              title={mainVideo?.name || 'movie_trailer'}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
               allowFullScreen
               frameBorder="0"

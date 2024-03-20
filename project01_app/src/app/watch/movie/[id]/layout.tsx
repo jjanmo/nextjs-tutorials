b07459a501b920e9d22068dac9758d4066f8aff2
jjanmo <jjanmo07@hanmail.net>
2024-03-21 08:36:31 +0900

    feat: 비디오 상세 하단 박스 레이아웃 추가

--- project01_app/src/app/watch/movie/[id]/layout.tsx
+++ project01_app/src/app/watch/movie/[id]/layout.tsx
@@ -8,16 +8,20 @@ interface Props {
 
 export default function MovieDetailPageLayout({ children, info, videos }: PropsWithChildren<Props>) {
   return (
-    <div className="flex flex-col min-w-min max-w-7xl px-6 mx-auto">
+    <div className="flex flex-col bg-emerald-50">
       {children}
-      <div className="flex">
+      <div className="max-w-[1170px] min-h-[800px] -mt-20 mx-auto pt-7 px-7 bg-[#060D18] rounded-2xl shadow-md z-10 text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ullam odit perspiciatis expedita cum
+        officiis voluptatum cumque ex maxime veniam facilis quo, quas ea eos provident. Error sunt porro doloribus?
+      </div>
+      {/* <div className="flex">
         <div className="w-1/2 bg-slate-300">
           <Suspense fallback={<Loader />}>{info}</Suspense>
         </div>
         <div className="w-1/2 bg-blue-300">
           <Suspense fallback={<Loader />}>{videos}</Suspense>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }

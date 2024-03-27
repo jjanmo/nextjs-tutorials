72eaab070fe00375f901eb02f9fe668b2198de70
jjanmo <jjanmo07@hanmail.net>
2024-03-27 23:45:37 +0900

    fix: 다음달 캘린더데이터 로직 추가

--- project01_app/src/utils/calendar.ts
+++ project01_app/src/utils/calendar.ts
@@ -2,22 +2,30 @@ import { CalendarRenderingData } from '@/interface/calendar';
 import dayjs from 'dayjs';
 
 export const generateCalendar = (data: CalendarRenderingData) => {
-  const { startOfDay, totalDays, year, month } = data;
+  const { startOfDay, endOfDay, totalDays, year, month } = data;
 
-  const monthData = Array.from({ length: totalDays }, (_, i) => i + 1).map((date) => ({
+  const currentMonthData = Array.from({ length: totalDays }, (_, i) => i + 1).map((date) => ({
     date, // 날짜
     day: dayjs(`${year}-${month + 1}-${date}`).day(), // 요일
+    type: 'current',
   }));
 
   const endDayOfPrevMonth = getEndDayOfPrevMonth({ year, month });
   const startDateOfPrevMonth = endDayOfPrevMonth - startOfDay + 1;
 
-  const preMonthData = Array.from({ length: startOfDay }, (_, i) => i).map((day) => ({
+  const prevMonthData = Array.from({ length: startOfDay }, (_, i) => i).map((day) => ({
     date: startDateOfPrevMonth + day,
     day,
+    type: 'prev',
   }));
 
-  return [...preMonthData, ...monthData];
+  const nextMonthData = Array.from({ length: 6 - endOfDay }, (_, i) => i + 1).map((date) => ({
+    date,
+    day: endOfDay + date,
+    type: 'next',
+  }));
+
+  return [...prevMonthData, ...currentMonthData, ...nextMonthData];
 };
 
 export const getEndDayOfPrevMonth = ({ year, month }: { year: number; month: number }) => {

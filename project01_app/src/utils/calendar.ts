9d970baa07ad8bcf7b706b06e8e547212cdf4c71
jjanmo <jjanmo07@hanmail.net>
2024-03-27 23:58:46 +0900

    fix: 캘린더 랜더링 로직 관련 타이핑 수정

--- project01_app/src/utils/calendar.ts
+++ project01_app/src/utils/calendar.ts
@@ -1,29 +1,33 @@
-import { CalendarRenderingData } from '@/interface/calendar';
+import { CalendarData, CalendarRenderingCellData } from '@/interface/calendar';
 import dayjs from 'dayjs';
 
-export const generateCalendar = (data: CalendarRenderingData) => {
+export const generateCalendarRenderingData = (data: CalendarData) => {
   const { startOfDay, endOfDay, totalDays, year, month } = data;
 
-  const currentMonthData = Array.from({ length: totalDays }, (_, i) => i + 1).map((date) => ({
-    date, // 날짜
-    day: dayjs(`${year}-${month + 1}-${date}`).day(), // 요일
-    type: 'current',
-  }));
+  const currentMonthData: CalendarRenderingCellData[] = Array.from({ length: totalDays }, (_, i) => i + 1).map(
+    (date) => ({
+      date, // 날짜
+      day: dayjs(`${year}-${month + 1}-${date}`).day(), // 요일
+      type: 'current',
+    })
+  );
 
   const endDayOfPrevMonth = getEndDayOfPrevMonth({ year, month });
   const startDateOfPrevMonth = endDayOfPrevMonth - startOfDay + 1;
 
-  const prevMonthData = Array.from({ length: startOfDay }, (_, i) => i).map((day) => ({
+  const prevMonthData: CalendarRenderingCellData[] = Array.from({ length: startOfDay }, (_, i) => i).map((day) => ({
     date: startDateOfPrevMonth + day,
     day,
     type: 'prev',
   }));
 
-  const nextMonthData = Array.from({ length: 6 - endOfDay }, (_, i) => i + 1).map((date) => ({
-    date,
-    day: endOfDay + date,
-    type: 'next',
-  }));
+  const nextMonthData: CalendarRenderingCellData[] = Array.from({ length: 6 - endOfDay }, (_, i) => i + 1).map(
+    (date) => ({
+      date,
+      day: endOfDay + date,
+      type: 'next',
+    })
+  );
 
   return [...prevMonthData, ...currentMonthData, ...nextMonthData];
 };

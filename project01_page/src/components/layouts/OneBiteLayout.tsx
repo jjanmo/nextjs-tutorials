fc90c0493c4c6da80a29e308455a283c6aa3a309
jjanmo <jjanmo07@hanmail.net>
2024-10-12 02:26:20 +0900

    fix: 로고 링크 추가

--- project01_page/src/components/layouts/OneBiteLayout.tsx
+++ project01_page/src/components/layouts/OneBiteLayout.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from 'react'
-import SearchInput from '@/components/SearchInput'
+import SearchInput from '@/components/onebite/SearchInput'
 import styles from '@/styles/modules/Layout.module.css'
+import Link from 'next/link'
 
 const OneBiteLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className={styles.oneBiteLayoutContainer}>
-      <h1 className={styles.oneBiteLayoutTitle}>📚 onebite books</h1>
+      <Link href="/one-bite">
+        <h1 className={styles.oneBiteLayoutTitle}>📚 onebite books</h1>
+      </Link>
       <SearchInput />
       {children}
     </div>

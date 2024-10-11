c96f00360dc9a78f6b5e1e822595730d8c3b2a61
jjanmo <jjanmo07@hanmail.net>
2024-10-12 02:28:53 +0900

    feat: 북리스트 컴퍼넌트 구현 및 검색 후 페이지 이동 로직 추가

--- project01_page/src/components/onebite/SearchInput.tsx
+++ project01_page/src/components/onebite/SearchInput.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '@/styles/modules/OneBite.module.css'
+import { useRouter } from 'next/router'
 
 const SearchInput = () => {
+  const router = useRouter()
   const [text, setText] = useState('')
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,8 +18,14 @@ const SearchInput = () => {
     console.log(text)
     // 검색 API 호출
     // 검색 결과 페이지로 이동
-    setText('')
+    router.push(`/one-bite/search?q=${text}`)
   }
+
+  useEffect(() => {
+    const query = router.query.q as string
+    if (!query) setText('')
+    else setText(query)
+  }, [router.query.q])
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
--- project01_page/src/pages/one-bite/search/index.tsx
+++ project01_page/src/pages/one-bite/search/index.tsx
@@ -1,9 +1,20 @@
 import { useRouter } from 'next/router'
+import OneBiteLayout from '@/components/layouts/OneBiteLayout'
+import books from '@/mock/books.json'
+import BookList from '@/components/onebite/BookList'
 
 const Search = () => {
   const { query } = useRouter()
   console.log(query)
 
-  return <div>Search Page</div>
+  return (
+    <div>
+      <BookList books={books} />
+    </div>
+  )
 }
 export default Search
+
+Search.getLayout = (page: React.ReactNode) => {
+  return <OneBiteLayout>{page}</OneBiteLayout>
+}

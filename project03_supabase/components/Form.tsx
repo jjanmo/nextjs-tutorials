cddb06af015879d7f75b9aa53352310b7000d207
jjanmo <jjanmo07@hanmail.net>
2025-07-17 23:58:27 +0900

    feat: 수정/삭제/저장에 대한 로직 추가

--- project03_supabase/components/Form.tsx
+++ project03_supabase/components/Form.tsx
@@ -1,5 +1,5 @@
 import * as styles from '@/styles/form.css';
-import { FC, useState } from 'react';
+import { FC, FormEvent, useEffect, useRef, useState } from 'react';
 import { Note } from '@/types';
 
 interface Props {
@@ -7,33 +7,60 @@ interface Props {
 }
 
 const Form: FC<Props> = ({ note }) => {
-  const [title, setTitle] = useState(note?.title || '');
-  const [description, setDescription] = useState(note?.description || '');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [isEdit, setIsEdit] = useState(false);
+  const titleRef = useRef<HTMLInputElement>(null);
+
+  const handleEditClick = () => {
+    setIsEdit(true);
+    if (titleRef.current) {
+      titleRef.current.focus();
+    }
+  };
+  const handleSaveClick = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('save');
+    setIsEdit(false);
+  };
+  const handleDeleteClick = () => {
+    console.log('delete');
+  };
+
+  useEffect(() => {
+    if (!note) return;
+
+    setTitle(note.title);
+    setDescription(note.description);
+  }, [note]);
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSaveClick}>
       <input
+        ref={titleRef}
         className={styles.input}
         type="text"
         placeholder="노트의 제목을 입력하세요"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        readOnly={!isEdit}
       />
       <textarea
         className={styles.textarea}
         placeholder="노트의 내용을 입력하세요"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
+        readOnly={!isEdit}
       />
 
-      {note && (
-        <button className={styles.button} type="button">
+      {!isEdit && (
+        <button className={styles.button} type="button" onClick={handleEditClick}>
           수정
         </button>
       )}
-      {!note && (
+      {isEdit && (
         <div className={styles.buttonsContainer}>
-          <button className={styles.button} type="button">
+          <button className={styles.button} type="button" onClick={handleDeleteClick}>
             삭제
           </button>
           <button className={styles.button} type="submit">

02d8070d6fc3119c3b1bc9cc4a0ea1f319134c29
jjanmo <jjanmo07@hanmail.net>
2025-07-22 23:16:49 +0900

    feat: supabase 설정 및 API 연동

---
+++ project03_supabase/libs/supabase.ts
@@ -0,0 +1,39 @@
+import { Database } from '@/database.types';
+import { Note } from '@/types';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+export const getNotes = async () => {
+  try {
+    const { data: notes } = await supabase.from('note').select('*');
+    return notes;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const createNote = async (note: Note) => {
+  try {
+    const { error } = await supabase.from('note').insert(note);
+    if (error) throw error;
+    return true;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const patchNote = async (id: number, note: Note) => {
+  try {
+    const { data, error } = await supabase.from('note').update(note).eq('id', id).select();
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
--- project03_supabase/types/index.ts
+++ project03_supabase/types/index.ts
@@ -1,5 +1,4 @@
 export type Note = {
-  id: number;
   title: string;
-  description: string;
+  content: string;
 };

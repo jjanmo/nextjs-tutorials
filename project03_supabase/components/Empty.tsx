ae23eec35d3897c692c5400295538b40a25bf097
jjanmo <jjanmo07@hanmail.net>
2025-07-23 22:13:33 +0900

    feat: 빈화면 컴퍼넌트 추가

---
+++ project03_supabase/components/Empty.tsx
@@ -0,0 +1,11 @@
+import * as styles from '@/styles/empty.css';
+
+const EmptySection = () => {
+  return (
+    <div className={styles.container}>
+      <p>새로운 노트를 작성해보세요 😄</p>
+    </div>
+  );
+};
+
+export default EmptySection;
---
+++ project03_supabase/styles/empty.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  padding: 20,
+  backgroundColor: '#F1F1F1',
+});
